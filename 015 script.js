0d8ee2597a0704b83f87178f591e45df973b4b7c
galayevmurad@gmail.com <galayevmurad@gmail.com>
2022-02-21 17:58:55 +0400

    modify

--- 015 script.js
+++ 015 script.js
@@ -22,7 +22,7 @@ const personalMovieDB = {
    genres: [],
    privat: false
 };
-for (let i=1;i<2;i++) {
+for (let i=0;i<2;i++) {
     const a = prompt("Один из последних просмотренных фильмов?",""),
           b = prompt("На сколько оцените его?","");
     if (a != "" && b != "" && a != null && b != null && a.length < 50) {
